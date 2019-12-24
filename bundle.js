928f451014867e2c5b7d2df63d369dc708732f49
gamyrelidze <gavit.gamtrelidze19@gmail.com>
2019-12-24 12:24:32 +0400

    lab 9

--- bundle.js
+++ bundle.js
@@ -1,7 +1,7 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
-//amocana 1
+//davaleba 1
 var userForm = document.querySelector("#user-search");
 
 userForm.onsubmit = function (event) {
@@ -40,7 +40,7 @@ function submitHandler(event) {
       userPosts.appendChild(row);
     });
   });
-} //amocana 2
+} //davaleba 2
 
 
 var axios = require('axios');
